818c78d911506eb50a3d92077613c1a01f4e28c3
nicoacosta7 <nico.acosta7@gmail.com>
2025-05-16 15:39:57 -0300

    First commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home () {
+    return (
+        <>
+        <div>
+            <h1 className="text-3xl font-bold underline text-gray-700">Trivia Game</h1>
+        </div>
+        </>
+    )
+}
+
+export default Home;
