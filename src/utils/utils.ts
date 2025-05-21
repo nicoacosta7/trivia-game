7067dc2daf83bd76c5b1b32fe015e6849d0bd671
nicoacosta7 <nico.acosta7@gmail.com>
2025-05-20 23:10:49 -0300

    Clean code improvements

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 import type { Question, QuestionResponse } from "../types/gameTypes";
-
-export function transformQuestions(response: QuestionResponse[]): Question[] {
+ 
+function transformQuestions(response: QuestionResponse[]): Question[] {
     return response.map((q) => ({
         id: q.id,
         question: q.text,
@@ -9,7 +9,25 @@ export function transformQuestions(response: QuestionResponse[]): Question[] {
     }));
 }
 
-export function getRandomQuestions(questions: Question[], count: number): Question[] {
+function getRandomQuestions(questions: Question[], count: number): Question[] {
     const shuffled = [...questions].sort(() => Math.random() - 0.5);
     return shuffled.slice(0, count);
+}
+
+export async function getQuestions(questionCount: number) {
+    try {
+        const url = import.meta.env.VITE_QUESTIONS_URL;
+
+        const response = await fetch(url || "");
+
+        if (!response.ok) {
+            throw new Error("Error al obtener preguntas");
+        }
+
+        const data = await response.json();
+        const transformedQuestions = transformQuestions(data);
+        return getRandomQuestions(transformedQuestions, questionCount);
+    } catch (error) {
+        console.error("Error al obtener preguntas:", error);
+    }
 }
